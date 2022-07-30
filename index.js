17881380b6961a297c7a971fcca7b37d5096d52d
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-07-30 19:07:50 +0300

    Update exercices 3.x

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
@@ -45,8 +74,12 @@ app.get('/info', (req, res) => {
     )
 })
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+    Person
+      .find({})
+      .then(persons => {
+        response.json(persons)
+      })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -101,7 +134,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
--- mongo.js
+++ mongo.js
@@ -42,6 +42,3 @@ if (process.argv.length === 3) {
     })    
 }
 
-
-
-
